4c46341fc60ebc54fff57b69d6687d5ac53741da
TrophyBuck <cdbuck12@gmail.com>
2016-02-03 20:13:47 -0500

    Initial commit

---
+++ MainActivity.java
@@ -0,0 +1,13 @@
+package buck.christopher.project1_slots;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
